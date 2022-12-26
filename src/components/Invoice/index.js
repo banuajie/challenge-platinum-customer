fff000775babb9ac00ced386e2daec6a7be84d5f
Nugraha Banuajie <nugrahabanuajie@gmail.com>
2022-12-26 12:46:20 +0700

    perbaikan di bagian invoice

--- src/components/Invoice/index.js
+++ src/components/Invoice/index.js
@@ -10,6 +10,8 @@ import icon_success from "../../assets/images/icon_success.png";
 import icon_download from "../../assets/images/icon_download.png";
 import { useDispatch, useSelector } from "react-redux";
 import { getOrder } from "../../actions/orderAction";
+import jsPDF from "jspdf";
+import img_invoice from "../../assets/images/img_invoice.png";
 
 const Invoice = () => {
     const navigate = useNavigate();
@@ -26,6 +28,51 @@ const Invoice = () => {
             setUpdateOrder(getOrderResult);
         }
     }, [getOrderResult]);
+
+    console.log(updateOrder);
+
+    const handleDownload = (event) => {
+        event.preventDefault();
+
+        const doc = new jsPDF("landscape", "px", "a4", "false");
+
+        doc.addImage(img_invoice, "PNG", 65, 20, 500, 400);
+        doc.addPage();
+
+        doc.setFontSize("36");
+        doc.setFont("Arial", "bold");
+        doc.text(130, 60, "INVOICE BINAR CAR RENTAL");
+        doc.setFontSize("14");
+        doc.text(180, 80, "Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000");
+        doc.text(60, 100, "========================================================================================");
+
+        doc.setFontSize("14");
+        doc.text(60, 120, "ID Order");
+        doc.text(160, 120, `: ${updateOrder.id}`);
+
+        doc.text(60, 140, "Name Car");
+        doc.text(160, 140, `: ${updateOrder.Car.name}`);
+
+        doc.text(60, 160, "Email User");
+        doc.text(160, 160, `: ${updateOrder.User.email}`);
+
+        doc.text(60, 180, "Start Rent Date");
+        doc.text(160, 180, `: ${updateOrder.start_rent_at.slice("8", "10")} / ${updateOrder.start_rent_at.slice("5", "7")} / ${updateOrder.start_rent_at.slice("0", "4")}`);
+
+        doc.text(60, 200, "Finish Rent Date");
+        doc.text(160, 200, `: ${updateOrder.finish_rent_at.slice("8", "10")} / ${updateOrder.finish_rent_at.slice("5", "7")} / ${updateOrder.finish_rent_at.slice("0", "4")}`);
+
+        doc.text(60, 220, "Total Price");
+        doc.text(160, 220, `: Rp ${new Intl.NumberFormat("id-ID").format(updateOrder.total_price)}`);
+
+        doc.text(60, 240, "Status Pembayaran");
+        doc.text(160, 240, ": LUNAS");
+
+        doc.text(60, 260, "========================================================================================");
+        doc.text(60, 280, `Kelompok : Nugraha Banuajie dan Virna Oktarina`);
+
+        doc.save("invoice-binar-rental.pdf");
+    };
 
     return (
         <>
@@ -87,14 +134,14 @@ const Invoice = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col">
-                            {/* <form className="form-invoice p-3 shadow-sm w-50 mx-auto">
+                            <form className="form-invoice p-3 shadow-sm w-50 mx-auto">
                                 <div className="row">
                                     <div className="col align-self-center">
                                         <p className="text-invoice my-auto">Invoice</p>
                                     </div>
 
                                     <div className="col text-end">
-                                        <button className="button-download">
+                                        <button className="button-download" onClick={(event) => handleDownload(event)}>
                                             <div className="row">
                                                 <div className="col w-auto">
                                                     <img src={icon_download} alt="Download" className="my-2" />
@@ -110,7 +157,7 @@ const Invoice = () => {
                                 <div className="row">
                                     <div className="col"></div>
                                 </div>
-                            </form> */}
+                            </form>
                         </div>
                     </div>
                 </div>
