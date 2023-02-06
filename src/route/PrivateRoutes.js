3840c4687779b91690232c465eb9368095fc8857
Nugraha Banuajie <nugrahabanuajie@gmail.com>
2023-02-06 18:05:59 +0700

    hapus script yang tidak diperlukan

--- src/route/PrivateRoutes.js
+++ src/route/PrivateRoutes.js
@@ -5,12 +5,9 @@ const PrivateRoutes = () => {
     const location = useLocation();
     const token = window.localStorage.getItem("Token");
     const getSessionCarDetail = window.sessionStorage.getItem("SessionCarDetail");
-    // const getSessionSelectMethod = window.sessionStorage.getItem("SessionSelectMethod");
 
-    // if (token && getSessionCarDetail && getSessionSelectMethod) {
     if (token && getSessionCarDetail) {
         return <Outlet />;
-        // } else if (!getSessionCarDetail || !getSessionSelectMethod) {
     } else if (!getSessionCarDetail) {
         return <Navigate to="/cars" />;
     } else {
